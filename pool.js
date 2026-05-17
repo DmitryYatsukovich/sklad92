c4e593e4f7659c6d0f2f32c30c2c88db00dbf240
DmitryYatsukovich <dima.yatsukovich@mail.ru>
2026-05-17 17:42:04 +0300

    Add files via upload

---
+++ pool.js
@@ -0,0 +1,34 @@
+import pg from 'pg';
+
+const { Pool } = pg;
+
+function buildPoolConfig() {
+  const connectionString = process.env.DATABASE_URL;
+  const config = { connectionString };
+
+  const sslMode =
+    process.env.PGSSLMODE ||
+    process.env.DATABASE_SSLMODE ||
+    (connectionString?.match(/[?&]sslmode=([^&]+)/i)?.[1] ?? '');
+
+  if (sslMode === 'disable' || process.env.DATABASE_SSL === 'false') {
+    config.ssl = false;
+    return config;
+  }
+
+  const useSsl =
+    process.env.DATABASE_SSL === 'true' ||
+    ['require', 'verify-ca', 'verify-full', 'prefer'].includes(sslMode);
+
+  if (useSsl) {
+    config.ssl = {
+      rejectUnauthorized: process.env.DATABASE_SSL_REJECT_UNAUTHORIZED === 'true',
+    };
+  }
+
+  return config;
+}
+
+const pool = new Pool(buildPoolConfig());
+
+export default pool;
