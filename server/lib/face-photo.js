e3e3a971307798ce6de9c3648904939a3aa11c23
DmitryYatsukovich <dima.yatsukovich@mail.ru>
2026-05-24 23:29:38 +0300

    Delete server directory

--- server/lib/face-photo.js
+++
@@ -1,8 +0,0 @@
-/** @deprecated Используйте user-images.js — сохранение в PostgreSQL */
-export {
-  ensureFacePhotosDir,
-  facePhotoFilePath,
-  parseFaceImageBase64,
-  saveUserFacePhoto,
-  saveUserFacePhotoWithClient,
-} from './user-images.js';
